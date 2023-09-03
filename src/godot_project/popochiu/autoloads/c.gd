06a2c73b05a980ffec383a2a71c5bd6235167d2a
jeancallisti@yahoo.fr <jeancallisti@yahoo.fr>
2023-09-03 22:23:43 +0200

    imported assets for tibet mountain top

--- src/godot_project/popochiu/autoloads/c.gd
+++ src/godot_project/popochiu/autoloads/c.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_character.gd"
 
 # classes ----
+const PCIndy_indyclothes := preload('res://popochiu/characters/indy_indyclothes/character_indy_indyclothes.gd')
 # ---- classes
 
 # nodes ----
+var Indy_indyclothes: PCIndy_indyclothes : get = get_Indy_indyclothes
 # ---- nodes
 
 # functions ----
+func get_Indy_indyclothes() -> PCIndy_indyclothes: return super.get_runtime_character('Indy_indyclothes')
 # ---- functions
 
