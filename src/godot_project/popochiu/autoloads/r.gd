d77b5ef4b494987f8e27937bdc1c77650c294092
monsieur xx <jeancallisti@yahoo.fr>
2023-09-08 22:27:44 +0200

    finished deleting room 00, recreated room 00

--- src/godot_project/popochiu/autoloads/r.gd
+++ src/godot_project/popochiu/autoloads/r.gd
@@ -2,14 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 
 # classes ----
-const PR00_start_cutscene := preload('res://popochiu/rooms/00_start_cutscene/room_00_start_cutscene.gd')
+const PRR00_tibet_startCutscene := preload('res://popochiu/rooms/r00_tibet_start_cutscene/room_r00_tibet_start_cutscene.gd')
 # ---- classes
 
 # nodes ----
-var 00_start_cutscene: PR00_start_cutscene : get = get_00_start_cutscene
+var R00_tibet_startCutscene: PRR00_tibet_startCutscene : get = get_R00_tibet_startCutscene
 # ---- nodes
 
 # functions ----
-func get_00_start_cutscene() -> PR00_start_cutscene: return super.get_runtime_room('00_start_cutscene')
+func get_R00_tibet_startCutscene() -> PRR00_tibet_startCutscene: return super.get_runtime_room('R00_tibet_startCutscene')
 # ---- functions
 
